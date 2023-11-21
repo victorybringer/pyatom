0b96b10583828d9e9077588389099753e5d3ea2a
tim_j_yang <tim_j_yang@trendmicro.com>
2023-11-21 17:54:50 +0800

    fix

--- atomac/_a11y.py
+++ atomac/_a11y.py
@@ -203,7 +203,7 @@ class AXUIElement(object):
         else:
             return list(actions)
 
-    
+    @retry(stop_max_attempt_number=10)
     def _performAction(self, action):
         """
         Perform the specified action on the AXUIElement object
@@ -212,7 +212,7 @@ class AXUIElement(object):
         """
         err = AXUIElementPerformAction(self.ref, action)
 
-        if err != kAXErrorSuccess and err != kAXErrorCannotComplete and err != kAXErrorAttributeUnsupported :
+        if err != kAXErrorSuccess and err != kAXErrorCannotComplete :
             _setError(err, 'Error performing requested action')
     
 
